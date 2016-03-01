58680463c10cca2a223f717b4d72c96bb73798f0
ahsonkhan <ahson_ahmedk@yahoo.com>
2016-03-01 12:01:50 -0500

    Added playerData grab for NBA and formatting

--- WebCrawlPlayerData/src/webcrawlplayerdata/WebCrawlPlayerData.java
+++ WebCrawlPlayerData/src/webcrawlplayerdata/WebCrawlPlayerData.java
@@ -32,104 +32,101 @@ public class WebCrawlPlayerData {
     private final static String RDSUrl = "jdbc:mysql://fantasysports.cvut0y8wktov.us-west-2.rds.amazonaws.com:3306/fantasysports?connectTimeout=2000";
     private final static String RDSUser = "root";
     private final static String RDSPassword = "hamyharrymic";
-    
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-       try {
-           Class.forName("com.mysql.jdbc.Driver").newInstance();
-           conn = DriverManager.getConnection(RDSUrl, RDSUser, RDSPassword);
-       } catch (Exception e) {
-           System.out.println("exception");
-           System.out.println(e);
-       }
-       
-       PreparedStatement stmt = null;
-       ResultSet rs = null;
-       String query = "select count(*) as total from NBAPlayer";
-        System.out.println("Hello: ");
-		try {
-			
-			stmt = conn.prepareStatement(query);
-			rs = stmt.executeQuery();
-                        //System.out.println("Hello1: " + rs.getInt("total"));
-			if (rs.next()) {
-				System.out.println("Hello2: " + rs.getInt("total"));
-			}
-		} catch (Exception e) {
-                    System.out.println("except2");
-                    System.out.println(e);
-		}
-       
-       //return;
-                
-                
-        query = "INSERT INTO NBAPlayer (playerID, playerName) values (1, \"Kobe Bryant\");";
-        System.out.println("Hello: ");
-        try {
-                stmt = conn.prepareStatement(query);
-                stmt.executeUpdate();
-        } catch (Exception e) {
-            System.out.println("except2");
-            System.out.println(e);
+        if (!ConnectToDB() || conn == null) {
+            return;
         }
-        
-        
-        /*String rootUrl = "http://www.basketball-reference.com/";
-        
-        int[] requestedStats = {10, 11, 20, 23, 24, 25, 26, 29};
-        Map<Integer, String> statName = new HashMap<Integer, String>();
-        statName.put(10, "FG%");
-        statName.put(11, "3P");
-        statName.put(20, "FT%");
-        statName.put(23, "TRB");
-        statName.put(24, "AST");
-        statName.put(25, "STL");
-        statName.put(26, "BLK");
-        statName.put(29, "PTS");
-        
-        Document doc;
+
+        String rootUrl = "http://www.basketball-reference.com/";
+
+        ExtractAndPushPlayerData(rootUrl);
+    }
+
+    private static boolean ConnectToDB() {
         try {
-            for (int i = 0; i < 26; i++)
-            {
-                char alphabet = (char)(97 + i);
-                String playerListUrl = rootUrl + "players/" + alphabet + "/";
-                doc = Jsoup.connect(playerListUrl).get();
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            conn = DriverManager.getConnection(RDSUrl, RDSUser, RDSPassword);
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+        return true;
+    }
+
+    private static void ExtractAndPushPlayerData(String rootUrl) {
+        String separator;
+        int counter = 1;
+        StringBuilder query;
+        Document doc;
+
+        String playerListUrl = rootUrl + "players/";
+
+        for (int i = 0; i < 26; i++) {
+            try {
+                char alphabet = (char) (97 + i);
+                String playerListAlphabet = playerListUrl + alphabet + "/";
+                doc = Jsoup.connect(playerListAlphabet).get();
                 Element players = doc.getElementById("players");
                 Elements links = players.select("a[href]");
-                
+
+                Elements numPlayers = doc.select("h2");
+                String numOfPlayers = numPlayers.get(0).text();
+                numOfPlayers = numOfPlayers.replaceAll("[^\\d.]", "");
+                int numPlayersInt = Integer.parseInt(numOfPlayers);
+
+                query = new StringBuilder();
+                query.append("INSERT INTO NBAPlayer (playerID, playerName) values");
+                separator = "";
+
                 for (Element link : links) {
                     String playerLink = link.attr("abs:href");
-                    if (!playerLink.contains(playerListUrl)) continue;
-                    //System.out.println(playerLink);
-                    Document playerDoc = Jsoup.connect(playerLink).get();
-                    //Element name = playerDoc.getElementsByClass("person_image_offset").first();
-                    System.out.println(link.text());
-                    Element totals = playerDoc.getElementById("totals");
-                    Elements footer = totals.select("tfoot");
-                    Element data = footer.get(0);
-                    Element list = data.child(0);
-                    Elements items = list.select("td");
-                    
-                    // TODO: Push to DB instead of console.
-                    for (int statIndex : requestedStats)
-                    {
-                        System.out.print(statName.get(statIndex) + " - ");
-                        System.out.println(items.get(statIndex).text());
+                    if (!playerLink.contains(playerListUrl)) {
+                        continue;
                     }
-                    
-                    //System.out.println(alphabet);
+                    query.append(separator);
+                    separator = ",";
+                    query.append(" (");
+                    query.append(counter);
+                    query.append(", \"");
+                    query.append(link.text());
+                    query.append("\")");
+                    counter++;
                 }
-                
-                //System.out.println(alphabet);
+
+                query.append(";");
+
+                if (!PushToDB(query, numPlayersInt)) {
+                    System.out.println("Failed to submit query to DB: counter = " + counter + ", alphabet = " + alphabet + ".");
+                }
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
             }
-            
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-            //Logger.getLogger(WebCrawlPlayerData.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
+        }
     }
-    
+
+    private static boolean PushToDB(StringBuilder query, int verifyCount) {
+        if (conn == null) {
+            return false;
+        }
+
+        PreparedStatement stmt;
+
+        try {
+            stmt = conn.prepareStatement(query.toString());
+            int rowCount = stmt.executeUpdate();
+            if (verifyCount != rowCount) {
+                System.out.println("Mistake in query: rowCount = " + rowCount + ", verifyCount = " + verifyCount + ".");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+
+        return true;
+    }
+
 }
